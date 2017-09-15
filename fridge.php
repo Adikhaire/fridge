b1d1e0148036f5d86933c74592363d2f1ff2ee57
Aditya Khaire <u5657642@anu.edu.au>
2017-09-15 16:13:27 +1000

    conversion of date

--- fridge.php
+++ fridge.php
@@ -21,10 +21,13 @@ public function read_csv($data){
   $this->item = (string)$data[0];
   $this->amount = (int)$data[1];
   $this->unit = (string)$data[2];
-  $this->useby = date_create_from_format('d/m/y',$data[3]);
-}
-}
 
+  $strdate = $data[3];
+  $datecomp = explode("/",$strdate);
+  $dateStr = $datecomp[0]."-".$datecomp[1]."-".$datecomp[2];
+  $this->useby = $dateStr;
+}
+}
 
 $count = count($data);
 $list = array();
@@ -34,36 +37,58 @@ for($c=0; $c < $count;$c++) {
   $list[] = $fr;
 }
 
-foreach ($list as $obj ) {
-  #echo $obj->item."</br>";
+#foreach ($list as $obj ) {
+#  echo $obj->useby."</br>";
   #echo $obj->amount."</br>";
-}
+#}
 
 $str = file_get_contents("/Applications/XAMPP/xamppfiles/htdocs/fridge/recipe.json");
 $recipe = json_decode($str,TRUE);
-
-
 for ($a=0; $a< count($recipe);$a++){
   $name = $recipe[$a]['name'];
   $ingredientsCount = count($recipe[$a]['ingredients']);
-  $count_1 = 0;
+  $countingIngredients = 0;
   for ($b=0; $b< count($recipe[$a]['ingredients']);$b++){
-
     foreach($list as $obj){
       $ref = $recipe[$a]['ingredients'][$b];
-    if ($ref['item'] == $obj->item and (int)$ref['amount'] <= $obj->amount and $ref['unit'] == $obj->unit and $obj->useby <= date('d/m/y')){
-        $count_1 +=1;
-        print $count_1;
+      #if (strpos($ref['item'],str_replace(" ","",$obj->item))!==false){
+      #  print $ref['item'];
+      #}
+    if ( (strpos($ref['item'],str_replace(" ","",$obj->item))!==false) and (int)$ref['amount'] <= $obj->amount and $ref['unit'] == $obj->unit and strtotime($obj->useby) > time()){
+        $countingIngredients +=1;
+        print $countingIngredients."</br>";
     }
   }
-  if ($count_1 == $ingredientsCount){
+  if ($countingIngredients == $ingredientsCount){
     echo $name."</br>";
   }
 }
 
 }
 
-echo date('d/m/y');
+// $today = time();
+// print $today."</br>";
+// foreach ($list as $obj){
+//   $usebyDate = $obj->useby;
+//   print strtotime($usebyDate)."</br>";
+// if (strtotime($usebyDate) > $today ){
+//     echo $obj->useby."</br>";
+//     print $obj->item."</br>";
+//     print("not expired")."</br>";
+// } else{
+//     print("expired")."</br>";
+//     echo $obj->useby."</br>";
+//     print $obj->item."</br>";
+// }
+// }
+// foreach($list as $obj){
+//   if ($obj->useby.format('j/M/Y') < date('j/M/Y')){
+//     print "yes";
+//   }
+// }
+// print date('d/m/y');
+#and $obj->useby <= date('d/m/y')
+#echo date('d/m/y');
 #echo '<pre>' . print_r($recipe, true) . '</pre>';
 #var_dump($recipe[1]['ingredients'][1]['amount']);
 #var_dump($recipe[1]['name']);
